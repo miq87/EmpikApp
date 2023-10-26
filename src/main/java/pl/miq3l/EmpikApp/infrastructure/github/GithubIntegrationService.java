b60d5ff32bbf689f90ea0ffb3a44b238ec5d76ad
miq3l <miq3l@wp.pl>
2023-10-26 23:53:15 +0200

    wiremock added

--- src/main/java/pl/miq3l/EmpikApp/infrastructure/github/GithubIntegrationService.java
+++ src/main/java/pl/miq3l/EmpikApp/infrastructure/github/GithubIntegrationService.java
@@ -1,13 +1,10 @@
 package pl.miq3l.EmpikApp.infrastructure.github;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.miq3l.EmpikApp.configuration.properties.GithubIntegrationProperties;
-import pl.miq3l.EmpikApp.domain.user.converter.GithubUserResponseDtoToUserResponseDtoConverter;
 import pl.miq3l.EmpikApp.domain.userclick.model.UserClick;
 import pl.miq3l.EmpikApp.domain.userclick.service.UserClickService;
 import pl.miq3l.EmpikApp.dtos.GithubUserResponseDto;
@@ -17,20 +14,16 @@ import java.util.Objects;
 
 @Service
 public class GithubIntegrationService {
-    private final Logger LOG = LoggerFactory.getLogger(GithubIntegrationService.class);
     private final GithubIntegrationProperties properties;
     private final UserClickService userClickService;
     private final RestTemplate restTemplate;
-    private final GithubUserResponseDtoToUserResponseDtoConverter converter;
 
     public GithubIntegrationService(GithubIntegrationProperties properties,
                                     UserClickService userClickService,
-                                    RestTemplate githubRestTemplate,
-                                    GithubUserResponseDtoToUserResponseDtoConverter converter) {
+                                    RestTemplate githubRestTemplate) {
         this.properties = properties;
         this.userClickService = userClickService;
         this.restTemplate = githubRestTemplate;
-        this.converter = converter;
     }
 
     public GithubUserResponseDto getUserFromGithub(String login) {
